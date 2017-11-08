61646c41cc373f8e59385607c480388e435092bb
Sridevi <nxy17640@ucmo.edu>
2017-11-08 15:45:33 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import {CoursesComponent} from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LanguageComponent } from './language/language.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule
